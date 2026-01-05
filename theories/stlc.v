136735d0117aac6a4fb9345863519dcfdd5ef26b
Yang Ming-Tian <i@skylee.xyz>
2026-01-05 20:36:44 +0800

    use SSReflect (#7)
    * use ssreflect
    
    * use named parameters
    
    * avoid implicit automation

--- theories/stlc.v
+++ theories/stlc.v
@@ -3,31 +3,30 @@ From Autosubst Require Import Autosubst.
 From Equations Require Import Equations.
 From stdpp Require Import relations (rtc(..), rtc_trans).
 From Hammer Require Import Tactics.
+From Corelib Require Import ssreflect.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
 
 (* STLC *)
 
-Inductive typ :=
-| Bool : typ
-| Arr : typ → typ → typ.
+Implicit Types
+  (x : var)
+  (b : bool).
 
-Declare Scope typ_scope.
-Delimit Scope typ_scope with typ.
-Bind Scope typ_scope with typ.
+Inductive typ :=
+| Bool
+| Arr (τ₁ τ₂ : typ).
+
+Implicit Types
+  (τ : typ).
 
 Inductive exp :=
-| Var : var → exp
-| BLit : bool → exp
-| Abs : typ → {bind exp} → exp
-| App : exp → exp → exp.
-
-Declare Scope exp_scope.
-Delimit Scope exp_scope with exp.
-Bind Scope exp_scope with exp.
-
-#[export] Instance Ids_exp : Ids exp. derive. Defined.
-#[export] Instance Rename_exp : Rename exp. derive. Defined.
-#[export] Instance Subst_exp : Subst exp. derive. Defined.
-#[export] Instance SubstLemmas_exp : SubstLemmas exp. derive. Qed.
+| Var x
+| BLit b
+| Abs τ (e : {bind exp})
+| App (e₁ e₂ : exp).
 
 Coercion Var : var >-> exp.
 Coercion BLit : bool >-> exp.
@@ -37,8 +36,6 @@ Notation "∅" := nil.
 
 Implicit Types
   (e v : exp)
-  (x : var)
-  (b : bool)
   (Γ : ctx)
   (σ : var → exp).
 
@@ -49,17 +46,24 @@ Inductive val : exp → Prop :=
 #[export]
 Hint Constructors typ exp val : core.
 
+(* Autosubst *)
+
+#[export] Instance Ids_exp : Ids exp. derive. Defined.
+#[export] Instance Rename_exp : Rename exp. derive. Defined.
+#[export] Instance Subst_exp : Subst exp. derive. Defined.
+#[export] Instance SubstLemmas_exp : SubstLemmas exp. derive. Qed.
+
 (* Operational Semantics *)
 
 Reserved Infix "↪" (at level 70).
 Inductive step : exp → exp → Prop :=
-| StepBeta : ∀ τ e v,
+| StepBeta τ e v :
     val v →
     (App (Abs τ e) v) ↪ e.[v/]
-| StepApp₁ : ∀ e₁ e₁' e₂,
+| StepApp₁ e₁ e₁' e₂ :
     e₁ ↪ e₁' →
     App e₁ e₂ ↪ App e₁' e₂
-| StepApp₂ : ∀ e₁ e₂ e₂',
+| StepApp₂ e₁ e₂ e₂' :
     val e₁ →
     e₂ ↪ e₂' →
     App e₁ e₂ ↪ App e₁ e₂'
@@ -74,24 +78,24 @@ Hint Constructors step rtc : core.
 
 Reserved Notation "Γ ∋ x : τ" (at level 65, x at next level).
 Inductive lookup : ctx → var → typ → Prop :=
-| LookupZero : ∀ τ Γ,
+| LookupZero τ Γ :
     τ :: Γ ∋ 0 : τ
-| LookupSucc : ∀ Γ τ₁ x τ₂,
+| LookupSucc Γ τ₁ x τ₂ :
     Γ ∋ x : τ₂ →
     τ₁ :: Γ ∋ S x : τ₂
 where "Γ ∋ x : τ" := (lookup Γ x τ).
 
 Reserved Notation "Γ ⊢ e : τ" (at level 65, e at next level).
 Inductive typing : ctx → exp → typ → Prop :=
-| TypingVar : ∀ Γ x τ,
+| TypingVar Γ x τ :
     Γ ∋ x : τ →
     Γ ⊢ x : τ
-| TypingBLit : ∀ Γ b,
+| TypingBLit Γ b :
     Γ ⊢ b : Bool
-| TypingAbs : ∀ Γ τ₁ e τ₂,
+| TypingAbs Γ τ₁ e τ₂ :
     τ₁ :: Γ ⊢ e : τ₂ →
     Γ ⊢ Abs τ₁ e : Arr τ₁ τ₂
-| TypingApp : ∀ Γ τ₁ τ₂ e₁ e₂,
+| TypingApp Γ τ₁ τ₂ e₁ e₂ :
     Γ ⊢ e₁ : Arr τ₁ τ₂ →
     Γ ⊢ e₂ : τ₁ →
     Γ ⊢ App e₁ e₂ : τ₂
@@ -103,16 +107,16 @@ Hint Constructors lookup typing : core.
 (* Semantic Typing *)
 
 Inductive blit_val : exp → Prop :=
-| BLitValBLit : ∀ b, blit_val b.
+| BLitValBLit b : blit_val b.
 
 Inductive abs_val : exp → Prop :=
-| AbsValAbs : ∀ τ e, abs_val (Abs τ e).
+| AbsValAbs τ e : abs_val (Abs τ e).
 
 #[export]
 Hint Constructors blit_val abs_val : core.
 
 Reserved Notation "e ∈ 𝒱⟦ τ ⟧".
-Equations val_rel e (τ : typ) : Prop := {
+Equations val_rel e τ : Prop := {
 | e, Bool := blit_val e;
 | e, (Arr τ₁ τ₂) := abs_val e ∧ ∀ v,
     v ∈ 𝒱⟦τ₁⟧ →
@@ -122,7 +126,7 @@ Equations val_rel e (τ : typ) : Prop := {
 Reserved Notation "σ ∈ 𝒢⟦ Γ ⟧".
 Inductive ctx_rel : (var → exp) → ctx → Prop :=
 | CtxRelNil : ids ∈ 𝒢⟦∅⟧
-| CtxRelCons : ∀ Γ σ τ v,
+| CtxRelCons Γ σ τ v :
     σ ∈ 𝒢⟦Γ⟧ →
     v ∈ 𝒱⟦τ⟧ →
     (v .: σ) ∈ 𝒢⟦τ :: Γ⟧
@@ -144,95 +148,73 @@ Notation "Γ ⊨ e : τ" := (sem_typing Γ e τ) (at level 65, e at next level).
 
 (* Fundamental Property *)
 
-Lemma ctx_rel_lookup_msubst_val_rel : ∀ Γ σ x τ,
-  σ ∈ 𝒢⟦Γ⟧ →
-  Γ ∋ x : τ →
-  (σ x) ∈ 𝒱⟦τ⟧.
-Proof.
-  intros.
-  generalize dependent x.
-  generalize dependent τ.
-  induction H; sauto lq: on.
-Qed.
-
-Lemma fundamental_property_var : ∀ Γ x τ,
+Lemma fundamental_property_var Γ x τ :
   Γ ∋ x : τ →
   Γ ⊨ x : τ.
-Proof with eauto.
-  unfold sem_typing.
-  intros.
+Proof.
+  rewrite/sem_typing=> Hlookup σ Hcrel.
   exists (σ x).
-  split...
-  eapply ctx_rel_lookup_msubst_val_rel...
+  split=> //.
+  move: x τ Hlookup.
+  by elim: Hcrel; sauto lq: on.
 Qed.
 
-Lemma fundamental_property_blit : ∀ Γ b,
+Lemma fundamental_property_blit Γ b :
   Γ ⊨ b : Bool.
 Proof.
-  hauto l: on.
+  by hauto l: on.
 Qed.
 
-Lemma val_rel_val : ∀ τ v,
+Lemma val_rel_val τ v :
   v ∈ 𝒱⟦τ⟧ →
   val v.
 Proof.
-  induction τ; sauto q: on.
+  by elim: τ; sauto q: on.
 Qed.
 
-Lemma fundamental_property_abs : ∀ Γ τ₁ e τ₂,
+Lemma fundamental_property_abs Γ τ₁ e τ₂ :
   τ₁ :: Γ ⊨ e : τ₂ →
   Γ ⊨ Abs τ₁ e : Arr τ₁ τ₂.
-Proof with eauto.
-  unfold sem_typing.
-  intros.
+Proof.
+  rewrite/sem_typing=> Hsem_typing σ Hcrel.
   exists ((Abs τ₁ e).[σ]).
-  repeat split...
-  intros.
-  specialize H with (v .: σ).
-  enough (e.[v .: σ] ∈ ℰ⟦τ₂⟧)...
-  unfold exp_rel in H2.
-  destruct H2 as [v0 [Hstep_v0 Hvrel_v0]].
+  repeat split=> //.
+  move=> v Hvrel.
+  move: Hsem_typing => /(_ (v .: σ)) Hsem_typing.
+  have [v0 [Hstep_v0 Hvrel_v0]] : e.[v .: σ] ∈ ℰ⟦τ₂⟧ by auto.
   exists v0.
-  split...
+  split=> //.
   apply rtc_l with (y := e.[up σ].[v/]).
-  - apply StepBeta.
-    eapply val_rel_val...
-  - asimpl...
+  - by eauto using StepBeta, val_rel_val.
+  - by asimpl.
 Qed.
 
-Lemma fundamental_property_app : ∀ Γ τ₁ τ₂ e₁ e₂,
+Lemma fundamental_property_app Γ τ₁ τ₂ e₁ e₂ :
   Γ ⊨ e₁ : Arr τ₁ τ₂ →
   Γ ⊨ e₂ : τ₁ →
   Γ ⊨ App e₁ e₂ : τ₂.
-Proof with eauto.
-  unfold sem_typing.
-  unfold exp_rel.
-  intros.
-  specialize (H σ H1).
-  specialize (H0 σ H1).
-  destruct H as [v1 [Hmsubst_v1 Hvrel_v1]].
-  destruct H0 as [v2 [Hmsubst_v2 Hvrel_v2]].
-  destruct Hvrel_v1 as [Haval_v1 Happ_v1].
-  specialize (Happ_v1 v2 Hvrel_v2).
-  destruct Happ_v1 as [v [Hmstep_v Hvrel_v]].
+Proof.
+  rewrite/sem_typing/exp_rel=> + + σ Hcrel.
+  move=> /(_ σ Hcrel) [v1 [Hmsubst_v1 [Haval_v1 Happ_v1]]].
+  move=> /(_ σ Hcrel) [v2 [Hmsubst_v2 Hvrel_v2]].
+  move: Happ_v1 => /(_ v2 Hvrel_v2) [v [Hmstep_v Hvrel_v]].
   exists v.
-  split...
+  split=> //.
   inversion Haval_v1; subst.
-  simpl.
-  apply rtc_trans with (y := App (Abs τ e) v2)...
-  apply rtc_trans with (y := App (Abs τ e) e₂.[σ])...
-  - induction Hmsubst_v1...
-  - induction Hmsubst_v2...
+  apply rtc_trans with (y := App (Abs τ e) v2) => //.
+  apply rtc_trans with (y := App (Abs τ e) e₂.[σ]); simpl.
+  - by elim: Hmsubst_v1; eauto.
+  - by elim: Hmsubst_v2; eauto.
 Qed.
 
-Theorem fundamental_property : ∀ Γ e τ,
+Theorem fundamental_property Γ e τ :
   Γ ⊢ e : τ →
   Γ ⊨ e : τ.
-Proof with eauto.
-  intros.
+Proof.
+  move=> H.
   induction H.
-  - apply fundamental_property_var...
-  - apply fundamental_property_blit.
-  - apply fundamental_property_abs...
-  - eapply fundamental_property_app...
+  - exact: fundamental_property_var.
+  - exact: fundamental_property_blit.
+  - exact: fundamental_property_abs.
+  - by apply: fundamental_property_app; eauto.
 Qed.
